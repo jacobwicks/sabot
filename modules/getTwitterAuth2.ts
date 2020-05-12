072c76df1e3105e4dfc627839f00a183f11302ae
jacobwicks <jacob.m.wicks@gmail.com>
2020-05-12 12:30:49 -0700

    some improvements

--- modules/getTwitterAuth2.ts
+++ modules/getTwitterAuth2.ts
@@ -1,54 +1,56 @@
 import fetch from 'node-fetch';
 import { apiKeys } from '../config.json';
+
+//get the consumer key and secret from config.json
 const { twitter } = apiKeys;
 
-const rawUrlEncode = (str: string) => {
-    //           note 1: This reflects PHP 5.3/6.0+ behavior
-    //           note 1: Please be aware that this function expects \
-    //           note 1: to encode into UTF-8 encoded strings, as found on
-    //           note 1: pages served as UTF-8
-    //        example 1: rawurlencode('Kevin van Zonneveld!')
-    //        returns 1: 'Kevin%20van%20Zonneveld%21'
-    //        example 2: rawurlencode('https://kvz.io/')
-    //        returns 2: 'https%3A%2F%2Fkvz.io%2F'
-    //        example 3: rawurlencode('https://www.google.nl/search?q=Locutus&ie=utf-8')
-    //        returns 3: 'https%3A%2F%2Fwww.google.nl%2Fsearch%3Fq%3DLocutus%26ie%3Dutf-8'
+//encode the key and secret
+const rfcKey = encodeURI(twitter.consumerKey);
+const rfcSecret = encodeURI(twitter.consumerSecret);
 
-    str = str + '';
-
-    // Tilde should be allowed unescaped in future versions of PHP (as reflected below),
-    // but if you want to reflect current
-    // PHP behavior, you would need to add ".replace(/~/g, '%7E');" to the following.
-    return encodeURIComponent(str)
-        .replace(/!/g, '%21')
-        .replace(/'/g, '%27')
-        .replace(/\(/g, '%28')
-        .replace(/\)/g, '%29')
-        .replace(/\*/g, '%2A');
-};
-
-const rfcKey = rawUrlEncode(twitter.consumerKey);
-const rfcSecret = rawUrlEncode(twitter.consumerSecret);
+//make the bearer token credentials string from the encoded key and secret
 const bearerTokenCredentials = `${rfcKey}:${rfcSecret}`;
+
+//encode the token credentials to base64
 const base64BearerTokenCredentials = Buffer.from(
     bearerTokenCredentials
 ).toString('base64');
 
+//the options object for the fetch request
 const options = {
     headers: {
+        //gzip indicates compressed data
         accept: 'gzip',
+
+        //the base64 encoded credentials
         Authorization: 'Basic ' + base64BearerTokenCredentials,
+
         'content-type': 'application/x-www-form-urlencoded',
     },
+
+    //it's a post request
     method: 'POST',
+
+    //tells OAuth you are asking for your own app's credentials
+    //not credentials on behalf of a user of your app
     body: 'grant_type=client_credentials',
 };
 
-const getTwitterToken = async () => {
+const getTwitterAuth2 = async () => {
+    //make the fetch request to the twitter api endpoint
     const response = await fetch(
         'https://api.twitter.com/oauth2/token',
         options
     );
+
+    //the json method on the response object is async
     const bearerToken = await response.json();
+
+    //now you've got the bearerToken {
+    //"token_type": "bearer",
+    //"access_token": "string goes here"
+    //}
     return bearerToken;
 };
+
+export default getTwitterAuth2;
